9eb218468f5204a272a9ec0bc07891f2a6bf3ee7
aquashdw <aquashdw@gmail.com>
2021-07-01 19:22:41 +0900

    generate basis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [],
+  imports: [PlayersModule],
   controllers: [AppController],
   providers: [],
 })
---
+++ src/players/entities/players.entity.ts
@@ -0,0 +1,23 @@
+export class Player {
+  id: number;
+  username: string;
+  name: string;
+  age: number;
+}
+
+export class Position {
+  id: number;
+  name: string;
+  positionNumber: number;
+}
+
+export class PitchHand {
+  id: number;
+  hand: string;
+  style: string;
+}
+
+export class SwingSide {
+  id: number;
+  side: string;
+}
