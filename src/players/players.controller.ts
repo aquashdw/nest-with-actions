151bf9c9a93cb58fa923e8d7fc3c1336be59eb1b
aquashdw <aquashdw@gmail.com>
2021-07-02 22:59:04 +0900

    add player dtos

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Player } from './entities/player.entity';
 
 @Controller('players')
-export class PlayersController {}
+export class PlayersController {
+  @Get()
+  readAllPlayers(): Player[] {
+    return null;
+  }
+
+  @Get('/:id')
+  readPlayer(@Param('id') playerId: number): Player {
+    return null;
+  }
+
+  @Post('')
+  createPlayer() {
+    return null;
+  }
+
+  @Patch('/:id')
+  updatePlayer(@Param('id') playerId: number) {
+    return null;
+  }
+
+  @Delete('/:id')
+  deletePlayer() {
+    return null;
+  }
+}
