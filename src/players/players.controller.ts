6f053c76554ae6db82d76ca11a613d51cda72d64
aquashdw <aquashdw@gmail.com>
2021-07-06 17:31:47 +0900

    player controller methods

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,30 +1,51 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { Player } from './entities/player.entity';
+import { PlayersService } from './players.service';
+import { CreatePlayerDto } from './dto/create-player.dto';
+import { UpdatePlayerDto } from './dto/update-player.dto';
 
 @Controller('players')
 export class PlayersController {
+  constructor(private readonly playersService: PlayersService) {}
+
   @Get()
-  readAllPlayers(): Player[] {
-    return null;
+  async readAllPlayers(): Promise<Player[]> {
+    return await this.playersService.findAllPlayers();
   }
 
   @Get('/:id')
-  readPlayer(@Param('id') playerId: number): Player {
-    return null;
+  async readPlayer(@Param('id') playerId: number): Promise<Player> {
+    return await this.playersService.findOne(playerId);
   }
 
   @Post('')
-  createPlayer() {
-    return null;
+  @HttpCode(HttpStatus.CREATED)
+  async createPlayer(@Body() createPlayerDto: CreatePlayerDto) {
+    return await this.playersService.createPlayer(createPlayerDto);
   }
 
   @Patch('/:id')
-  updatePlayer(@Param('id') playerId: number) {
-    return null;
+  @HttpCode(HttpStatus.ACCEPTED)
+  async updatePlayer(
+    @Param('id') playerId: number,
+    updatePlayerDto: UpdatePlayerDto,
+  ) {
+    return await this.playersService.updatePlayer(playerId, updatePlayerDto);
   }
 
   @Delete('/:id')
-  deletePlayer() {
-    return null;
+  @HttpCode(HttpStatus.ACCEPTED)
+  async deletePlayer(@Param('id') playerId: number) {
+    return await this.playersService.deletePlayer(playerId);
   }
 }
