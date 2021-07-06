b85ff4930e6d48c152799a83081a060c41a56d7e
aquashdw <aquashdw@gmail.com>
2021-07-06 17:27:33 +0900

    add typeorm to players

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PlayersService } from './players.service';
 import { PlayersController } from './players.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Player } from './entities/player.entity';
+import { Region } from '../region/entities/region.entity';
+import { Position } from './entities/position.entity';
+import { PitchStyle } from './entities/pitch.entity';
+import { BattingSide } from './entities/batting.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Player,
+      Position,
+      PitchStyle,
+      BattingSide,
+      Region,
+    ]),
+  ],
   controllers: [PlayersController],
   providers: [PlayersService],
 })
